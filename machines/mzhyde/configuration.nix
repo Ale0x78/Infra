08647a76114569330b22850900681f4c8a06d1be
Ale0x78 <anahape@ncsu.edu>
2024-08-03 17:31:24 -0400

    udev stuff... again

--- machines/mzhyde/configuration.nix
+++ machines/mzhyde/configuration.nix
@@ -59,7 +59,7 @@
   # Disable the motherboards's builtin bluetooth
   # Thanks https://discourse.nixos.org/t/how-to-disable-the-bluetooth-of-my-intel-wifi-card/40407/4
     services.udev.extraRules = ''
-      SUBSYSTEM=="usb", ATTR{idVendor}=="13d3", ATTR{idProduct}=="3533", ATTR{authorized}="0";
+      SUBSYSTEM=="usb", ATTR{idVendor}=="13d3", ATTR{idProduct}=="3533", ATTR{busnum}=="1", ATTR{authorized}="0";
     '';
     # SUBSYSTEM=="usb", ATTR{idVendor}=="8087", ATTR{idProduct}=="0a2a",
 
