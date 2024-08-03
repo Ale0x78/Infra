fbfff7d3cb11820b892388908a14f5537622cd76
Ale0x78 <anahape@ncsu.edu>
2024-08-03 16:20:45 -0400

    Steam plz

--- os/gaming.nix
+++ os/gaming.nix
@@ -7,5 +7,6 @@ programs.nix-ld.enable = true; # Needed for Heroic launcher
   services.sunshine.enable = true;
   # programs.lutris.enable = true;
   
+  hardware.steam-hardware.enable = true;
   services.sunshine.package = pkgs.sunshine.override {cudaSupport = true;};
 }
