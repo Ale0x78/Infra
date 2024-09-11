eaf62774f40b2b66a568b59789872535ac5566ea
Alex <anahape@ncsu.edu>
2024-09-11 19:16:26 -0400

    Update gfx.nix

--- os/gfx.nix
+++ os/gfx.nix
@@ -2,12 +2,12 @@
 
 {
   services.xserver.enable = true;
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-  # services.xserver.desktopManager.gnome.extraGSettingsOverridePackages = [ pkgs.gnome.mutter ];
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.extraGSettingsOverridePackages = [ pkgs.gnome.mutter ];
 
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
   # sound.enable = true;
   programs.dconf.enable = true;
