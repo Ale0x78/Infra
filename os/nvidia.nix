b0d3f983a02690d488d1b4498e8e6e7cce62ceb9
Ale0x78 <anahape@ncsu.edu>
2024-11-17 11:58:41 -0500

    Un-Stability improvements

--- os/nvidia.nix
+++ os/nvidia.nix
@@ -34,7 +34,7 @@
     nvidiaSettings = false;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
     # package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
