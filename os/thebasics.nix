a6eb7f5206cda239ec712c8196eb615d4e135eac
Ale0x78 <anahape@ncsu.edu>
2024-12-30 15:38:08 -0500

    LazyNvim is not a basic

--- os/thebasics.nix
+++ os/thebasics.nix
@@ -2,7 +2,6 @@
 {
   environment.systemPackages = with pkgs; [
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-      vimPlugins.LazyVim
       wget
       helix
       neovim
