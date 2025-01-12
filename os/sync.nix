668303aee0fe29944ca3fbf39ec9b7bd08cb2a11
Ale0x78 <anahape@ncsu.edu>
2025-01-11 22:52:01 -0500

    Syncing selected folders

--- os/sync.nix
+++ os/sync.nix
@@ -7,8 +7,14 @@
         syncthing = {
             enable = true;
             user = "alex";
-            dataDir = "/home/alex/Ravka";    # Default folder for new synced folders
+            dataDir = "/home/alex/Sync";    # Default folder for new synced folders
             configDir = "/home/alex/.config/syncthing";   # Folder for Syncthing's settings and keys
+            "Ravka" = {
+                path = "/home/alex/Ravka";
+            };
+            "Fold" = {
+                path = "/home/alex/Fold";
+            };
         };
 };
 }
