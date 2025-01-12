54d49a60c11bef956ce32e4da08bec734dc741de
Ale0x78 <anahape@ncsu.edu>
2025-01-11 22:43:15 -0500

    Hello pipewire... again

--- os/gfx.nix
+++ os/gfx.nix
@@ -10,11 +10,19 @@
   hardware = {
     steam-hardware.enable = true;
     bluetooth.enable = true;
-    pulseaudio.enable = false;
+    # pulseaudio.enable = false;
     # pulseaudio.support32Bit = true;
   };
 
   programs.river.enable = true;
-
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true; # if not already enabled
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
 
 }
