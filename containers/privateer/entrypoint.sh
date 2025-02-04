251cfbd5ae28714a44f3266b52851b2b1ce106c7
Ale0x78 <anahape@ncsu.edu>
2025-02-03 21:56:16 -0500

    I don't think parrot was a good fit

--- containers/privateer/entrypoint.sh
+++ containers/privateer/entrypoint.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 rm /tmp/.X1-lock 2> /dev/null &
 /opt/noVNC/utils/novnc_proxy --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &
-# Insecure option is needed to accept connections from the docker host.
 vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE &
 wait
