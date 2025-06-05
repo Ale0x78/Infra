d465e9c3fe7d3a78146afb11e7f68b3e29d526fd
Ale0x78 <anahape@ncsu.edu>
2025-06-04 22:35:33 -0400

    Adguard

--- machines/flamela/configuration.nix
+++ machines/flamela/configuration.nix
@@ -10,6 +10,7 @@
       ../../services/jellyfin.nix
       ../../services/localSSH.nix
       ../../services/grafana.nix
+      ../../services/adguard.nix
 
       ../../users/alex.nix
 
@@ -22,6 +23,14 @@
   time.timeZone = "US/Eastern";
   networking.hostId = "5677ca84";
 
+  networking = {
+    interfaces = {
+      enp4s0.ipv4.addresses = [{
+      address = "192.168.86.24";
+      prefixLength = 24;
+      }];
+    };
+  };
 
   system.stateVersion = "25.05"; # Did you read the comment? No I deleted it; tldr, do not remove
 
---
+++ services/adguard.nix
@@ -0,0 +1,9 @@
+{ config, lib, pkgs, ... }:
+{
+  services.adguardhome = {
+    enable = true;
+  };
+  environment.systemPackages = with pkgs; [
+    adguardian
+  ];
+}
