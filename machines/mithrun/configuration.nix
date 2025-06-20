ad030ed1938581836d045e7dd91b25334a027cb2
Ale0x78 <anahape@ncsu.edu>
2025-06-20 11:31:49 -0400

    No more cache

--- machines/mithrun/configuration.nix
+++ machines/mithrun/configuration.nix
@@ -26,14 +26,6 @@
       ../../services/sync.nix
       ../../services/python3.nix
     ];
-  nix.settings = {
-    substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
   networking.hostId = "d5abb711";
   specialisation."VFIO".configuration = {
    system.nixos.tags = [ "with-vfio" ];
